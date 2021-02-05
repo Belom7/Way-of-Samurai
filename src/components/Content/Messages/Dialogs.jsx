a755dc2336f36cf08a3c067c7a7ef51725601770
Belom7 <Still07pkm@gmail.com>
2021-02-05 08:41:21 +0300

    рефакторил код для Dialogs и Comments вынес данные типа имя, id, сообщение, кол-во лайков итд в переменную которая потом передается в пропсы

--- src/components/Content/Messages/Dialogs.jsx
+++ src/components/Content/Messages/Dialogs.jsx
@@ -3,22 +3,38 @@ import Classes from './Dialogs.module.css';
 import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 
+let dialogsData = [
+    {id: 1, name: 'Maks'},
+    {id: 2, name: 'Evgen'},
+    {id: 3, name: 'Andrey'},
+    {id: 4, name: 'Ivan'},
+    {id: 5, name: 'Egor'}
+]
+
+let messageData = [
+    {id: 1, message: 'Привет!'},
+    {id: 2, message: 'Погнали гулять!?'},
+    {id: 3, message: 'Чего делаешь?'},
+    {id: 4, message: 'ЭЭЭ ау'},
+    {id: 5, message: 'гГ'},
+]
+
 const Dialogs = (props) => {
     return(
         <div className={Classes.dialogs}>
             <div className={Classes.dialogsItem}> 
-                <Dialog name='Maks' id='1'/>
-                <Dialog name='Evgen' id='2'/>
-                <Dialog name='Andrey' id='3'/>
-                <Dialog name='Ivan' id='4'/>
-                <Dialog name='Egor' id='5'/>                
+                <Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <Dialog name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <Dialog name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <Dialog name={dialogsData[4].name} id={dialogsData[4].id}/>                
             </div>
             <div className={Classes.messages}>
-                <Message message='Привет!'/>
-                <Message message='Погнали гулять!?'/>
-                <Message message='Чего делаешь?'/>
-                <Message message='ЭЭЭ ау!'/>
-                <Message message='гГ'/>
+                <Message message={messageData[0].message} id={messageData[0].id}/>
+                <Message message={messageData[1].message} id={messageData[1].id}/>
+                <Message message={messageData[2].message} id={messageData[2].id}/>
+                <Message message={messageData[3].message} id={messageData[3].id}/>
+                <Message message={messageData[4].message} id={messageData[4].id}/>
             </div>
         </div>
     )
--- src/components/Content/News/Comments/Comments.jsx
+++ src/components/Content/News/Comments/Comments.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import classes from './Comments.module.css'
+import Classes from './Comments.module.css'
 import Comment from './Comment/Comment';
 
-function Comments() {
+let postsData = [
+  {id= 1, name: 'Maksim', message:'Навальный жгет!', like: 2},
+  {id= 2, name: 'Ivan', message:'Ля, фига се!', like: 15}
+]
+
+const Comments = (props) => {
     return(
-        <div className={classes.MyPosts}>
-          <p>Комментарии</p>
-          <Comment name='Maksim' massage='Навальный жгет!' like='2'/>
-          <Comment name='Ivan' massage='Ля, фига се' like='15'/>
+        <div className={Classes.MyPosts}>
+          <h3>Комментарии</h3>
+          <Comment name={postsData[0].name} massage={postsData[0].message} like={postsData[0].like} id={postsData[0].id}/>
+          <Comment name={postsData[1].name} massage={postsData[1].message} like={postsData[1].like} id={postsData[1].id}/>
         </div>
     )
 }
