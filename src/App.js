b0d6daeff8528c4c821812b3b67ff47f48f9eab3
Belom7 <Still07pkm@gmail.com>
2021-02-19 21:59:56 +0300

    удалили render.js, паттерн observer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Content from './components/Content/Content'
 
-function App(props) {
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
-import {render} from './render';
+import state, { subscribe } from './redux/state';
+import ReactDOM from 'react-dom';
+import {addPost, UpdateNewPost} from './redux/state';
+
+let render =(state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} UpdateNewPost={UpdateNewPost}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 render(state);
+subscribe(render);
 
 
 // If you want to start measuring performance in your app, pass a function
