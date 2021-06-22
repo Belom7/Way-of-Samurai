7531a411831127b7503bb5511e15457686edd897
Belom7 <Still07pkm@gmail.com>
2021-06-23 00:35:08 +0300

    зарефакторили код в компоненте Users, из презентационной компоненты переписали ее в классовую

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -3,37 +3,43 @@ import React from 'react';
 import Classes from './Users.module.css'
 import UserImg from '../../../assets/images/user.png'
 
-const Users = (props) => {
-    if(props.users.length === 0) {
+class User extends React.Component {
+
+    constructor(props) {
+        super(props);
+
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUser(response.data.items)
+            this.props.setUser(response.data.items)
         })
     }
-    return(
-        <div>
-            {
-                props.users.map( u => <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={ u.photos.small != null ? u.photos.small : UserImg} className={Classes.photo} alt='Аватарка'/>
-                        </div>
-                        <div>
-                            {u.followed ? <button onClick ={() => {props.unfollow(u.id)}}>Follow</button> 
-                            : <button onClick ={() => {props.follow(u.id)}}>Unfollow</button>}
-                        </div>
-                    </span>
-                    <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    {/* <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.sity}</div>
-                    </span> */}
-                </div>)
-            }
-        </div>
-    )
+    
+    render(){
+        return(
+            <div>
+                {
+                    this.props.users.map( u => <div key={u.id}>
+                        <span>
+                            <div>
+                                <img src={ u.photos.small != null ? u.photos.small : UserImg} className={Classes.photo} alt='Аватарка'/>
+                            </div>
+                            <div>
+                                {u.followed ? <button onClick ={() => {this.props.unfollow(u.id)}}>Follow</button> 
+                                : <button onClick ={() => {this.props.follow(u.id)}}>Unfollow</button>}
+                            </div>
+                        </span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        {/* <span>
+                            <div>{u.location.country}</div>
+                            <div>{u.location.sity}</div>
+                        </span> */}
+                    </div>)
+                }
+            </div>
+        )
+    }
 }
 
-export default Users;
+export default User;
