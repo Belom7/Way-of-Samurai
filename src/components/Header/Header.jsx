17fd6e83f3599742b086f03efb2121b789733c36
Belom7 <Still07pkm@gmail.com>
2021-07-03 17:49:11 +0300

    изучили что такое cookie, провели логинизацию сайта, сделали запрос с сервера зарегистрирован ли пользователь и вывели его имя на страницу

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
     return (
-        <div className= {Classes.header}>
-        <img src='http://s4.fotokto.ru/photo/full/331/3316553.jpg'></img>
-      </div>
+        <header className= {Classes.header}>
+          <img src='http://s4.fotokto.ru/photo/full/331/3316553.jpg' alt='Картинка хедера'></img>
+          <div className={Classes.loginBlock}>
+            {props.isAuth ? props.login : <NavLink to={'/Login'}>Login</NavLink>} 
+          </div>
+        </header>
     )
 }
 
