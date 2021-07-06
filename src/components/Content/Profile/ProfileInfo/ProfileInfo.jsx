cd086a82ef5015d12e7a3f0e22485395563861f2
Belom7 <Still07pkm@gmail.com>
2021-07-06 15:42:22 +0300

    сделали компоненту ProfileStatus которая отображает статус пользователя, она классовая, так же сделали локальный state, со всеми вытекающими

--- src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Classes from './ProfileInfo.module.css';
 import Preloader from './../../../common/preloader/Preloader'
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
     if(!props.profile) {
@@ -10,6 +11,7 @@ const ProfileInfo = (props) => {
         <div className={Classes.profileInfo}>
             <div>
                 <img src={props.profile.photos.large} alt='Аватарка большая' />
+                <ProfileStatus status={'Хай писюны!'}/>
                 <div>{props.profile.fullName}</div>
                 <div>{props.profile.aboutMe}</div>
             </div>
