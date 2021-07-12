8f614f1f3a0b25e8e721fac4fd57c941bf72f1a1
Belom7 <Still07pkm@gmail.com>
2021-07-12 14:13:52 +0300

    подключили библиотеку ReduxForm, вынесли рендер страницы Логина отдельной компонентой, в которой сделали форму (Form) с полями ввода Login/Password, чекбоксом remember me и кнопкой Login, используем Филды (Field) что бы не писать onChange (по ути это контейнерная компонента которая отрисовывает в себе другую компоненту), обернули презентационную компоненту ReduxForm(ом) который создаст HOC ( контейнерную компоненту),  закомбайнили formReducer в redux-store, теперь благадоря Формам собирается информация с полей ввода

--- src/components/Content/Login/Login.jsx
+++ src/components/Content/Login/Login.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import LoginReduxForm from './LoginForm';
 
-const Login = () => {
-    return <h1>Login</h1>
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return(<div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>   
+    )  
 }
 
-export default Login;
+export default Login
