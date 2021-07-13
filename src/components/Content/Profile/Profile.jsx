b4ecd8ffa9fd0f8cc14fcee6e737ef4174bf9961
Belom7 <Still07pkm@gmail.com>
2021-07-13 20:18:38 +0300

    сделали форму комментариям

--- src/components/Content/Profile/Profile.jsx
+++ src/components/Content/Profile/Profile.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import Classes from './Profile.module.css';
 import Posts from './Posts/Posts'
 import ProfileInfo from './ProfileInfo/ProfileInfo'
+import ProfileReduxForm from './ProfileInfo/ProfileCommentForm';
 
 
 const Profile = (props) => {
     
-    let addPost = () => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    
+    /* let addPost = () => {
         props.addPost();
     };
 
     let onPostChange = (e) => {
         let text = e.target.value
         props.onPostChange(text)
-    };
+    }; */
     
     return(
         <div className={Classes.profile}>
             <ProfileInfo profile={props.profile.profile} status={props.status} updateStatus={props.updateStatus}/>
-            <div>
-                <textarea onChange={onPostChange} value={props.profile.posts.newPostText}/>
-                <button onClick={addPost}>add post</button>
-            </div>
-                <Posts posts={props.profile.posts}/>
+            <ProfileReduxForm onSubmit={onSubmit}/>
+            <Posts posts={props.profile.posts}/>
         </div>
     )
 }
