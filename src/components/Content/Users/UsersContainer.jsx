2089ee6d64fed26a7e904e140b507b7c7e6033e7
Belom7 <Still07pkm@gmail.com>
2021-07-21 23:54:44 +0300

    сделали селекторы для UserContainer в MSTP (mapStateToProps)

--- src/components/Content/Users/UsersContainer.jsx
+++ src/components/Content/Users/UsersContainer.jsx
@@ -5,6 +5,7 @@ import Users from './Users';
 import Preloader from '../../common/preloader/Preloader';
 import { withAuthRedirect } from '../../../hoc/withAuthRedirect'
 import { compose } from 'redux';
+import { getCurrentPage, getFollowingInProgress, getIsFetching, getPageSize, getTotalUsersCount, getUsers } from '../../../redux/usersSelectors';
 
 
 class UsersContainer extends React.Component {
@@ -35,7 +36,7 @@ class UsersContainer extends React.Component {
 }
 
 
-let mapStateToProps = (state) => {
+/* let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
@@ -43,6 +44,17 @@ let mapStateToProps = (state) => {
         currentPage: state.usersPage.currentPage,
         isFetching: state.usersPage.isFetching,
         followingInProgress: state.usersPage.followingInProgress,
+    }
+} */
+
+let mapStateToProps = (state) => {
+    return {
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state),
     }
 }
 
