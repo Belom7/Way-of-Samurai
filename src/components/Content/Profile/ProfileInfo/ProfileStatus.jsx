379b510939eb5800560a7cce8353e4ff77d93c4c
Belom7 <Still07pkm@gmail.com>
2021-07-26 20:45:40 +0300

    переписали классовую компоненту ProfileStatus на функциональную с Хуком но пока не добавили замену жизненому циклу ComponentDidUpdate

--- src/components/Content/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,51 +1,68 @@
 import React from 'react';
+import { useState } from 'react';
 
-class ProfileStatus extends React.Component {
+const ProfileStatus = (props) =>{
 
-    state ={
-        editMode: false,
-        status: this.props.status,
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+
+    const activeitedEditMode = () => {
+        setEditMode(true)    
     }
-
-    activeitedEditMode = () => {
-        this.setState({
-            editMode : true,
-        })
+    const deActiveitedEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
         
     }
-    deActiveitedEditMode = () => {
-        this.setState({
-            editMode : false
-        })
-        this.props.updateStatus(this.state.status)
-    }
-
-    onStatusChange = (e) => {
-        this.setState({
-            status: e.currentTarget.value
-        })
-        
-    }
-
-
-    componentDidUpdate(prevProps, prevState){
-        if(prevProps.status !== this.props.status){
-            this.setState({status: this.props.status})
-        }
-    }
-
-    render(){
-        console.log('11')
     return(
         <div>
-            {!this.state.editMode && 
-                <div><span onClick={this.activeitedEditMode}>{this.props.status || 'No status'}</span></div>
+            {!editMode && 
+                <div><span onClick={activeitedEditMode}>{status || 'No status'}</span></div>
             }
-            {this.state.editMode && 
-                <div><input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deActiveitedEditMode} value={this.state.status}/></div>
+            {editMode && 
+                <div><input onChange={onStatusChange} autoFocus={true} onBlur={deActiveitedEditMode} value={status}/></div>
             }
         </div>
-    )}
+    )
 }
 
 export default ProfileStatus;
+
+
+
+
+
+/* state ={
+    editMode: false,
+    status: this.props.status,
+}
+
+activeitedEditMode = () => {
+    this.setState({
+        editMode : true,
+    })
+    
+}
+deActiveitedEditMode = () => {
+    this.setState({
+        editMode : false
+    })
+    this.props.updateStatus(this.state.status)
+}
+
+onStatusChange = (e) => {
+    this.setState({
+        status: e.currentTarget.value
+    })
+    
+}
+
+
+componentDidUpdate(prevProps, prevState){
+    if(prevProps.status !== this.props.status){
+        this.setState({status: this.props.status})
+    }
+} */
