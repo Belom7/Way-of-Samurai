ede3c13680804e5ee3958ba77fde54502afbc96b
Belom7 <Still07pkm@gmail.com>
2021-08-05 22:29:13 +0300

    сделали lazy загрузку так же обернли все роуты suspence и пока выполняется загрузка будет отображаться прелоадер

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import classes from './Content.module.css';
 import {Route, withRouter} from 'react-router-dom';
-import MessagesContainer from './Messages/MessagesContainer';
-import ProfileContainer from './Profile/ProfileContainer';
 import UsersContainer from './Users/UsersContainer'
-import News from './News/News';
-import Music from './Music/Music';
-import Login from './Login/Login';
 import { connect } from 'react-redux';
 import {initializeContent} from './../../redux/contentReducer'
 import { compose } from 'redux';
 import Preloader from '../common/preloader/Preloader';
+
+const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'));
+const News = React.lazy(() => import('./News/News'));
+const Music = React.lazy(() => import('./Music/Music'));
+const Login = React.lazy(() => import('./Login/Login'));
+const MessagesContainer = React.lazy(() => import('./Messages/MessagesContainer'));
 
 class Content extends React.Component { 
 
@@ -24,12 +25,14 @@ class Content extends React.Component {
     }
     return(      
       <div className={classes.Content}>
-        <Route path='/Profile/:userId?' render={ () => <ProfileContainer/>}/>
-        <Route path='/Messages' render={ () => <MessagesContainer/>}/>
-        <Route path='/News' render={ () => <News/>}/>
-        <Route path='/Music' render={ () => <Music/>}/>
-        <Route path='/Users' render={ () => <UsersContainer/>}/>
-        <Route path='/Login' render={ () => <Login/>}/>
+        <React.Suspense fallback={<Preloader/>}>
+          <Route path='/Profile/:userId?' render={ () => <ProfileContainer/>}/>
+          <Route path='/Messages' render={ () => <MessagesContainer/>}/>
+          <Route path='/News' render={ () => <News/>}/>
+          <Route path='/Music' render={ () => <Music/>}/>
+          <Route path='/Users' render={ () => <UsersContainer/>}/>
+          <Route path='/Login' render={ () => <Login/>}/>
+        </React.Suspense>
       </div>
     )
   }
